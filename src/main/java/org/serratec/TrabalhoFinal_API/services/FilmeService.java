62adaf183e95395d01697f38875bc5b23d906a68
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-22 14:04:40 -0300

    Criando metodo listar todos os filmes

--- src/main/java/org/serratec/TrabalhoFinal_API/services/FilmeService.java
+++ src/main/java/org/serratec/TrabalhoFinal_API/services/FilmeService.java
@@ -7,11 +7,33 @@ import org.serratec.TrabalhoFinal_API.repository.FilmeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class FilmeService {
 
     @Autowired
     private FilmeRepository filmeRepository;
+
+    public List<FilmeResponseDTO> listarFilmes(){
+        List<Filme> listaFilmes = filmeRepository.findAll();
+        List<FilmeResponseDTO> listaFilmeResponseDTO = new ArrayList<>();
+
+        for (Filme filme : listaFilmes) {
+            FilmeResponseDTO filmeResponseDTO = new FilmeResponseDTO(
+                    filme.getId(),
+                    filme.getTitulo(),
+                    filme.getDescricao(),
+                    filme.getDuracao(),
+                    filme.getDataLancamento(),
+                    filme.getNotaMedia(),
+                    filme.getClassificacaoIndicativa()
+            );
+        }
+
+        return listaFilmeResponseDTO;
+    }
 
     public FilmeResponseDTO criarFilme(FilmeRequestDTO dto){
         var novoFilme = new Filme();
