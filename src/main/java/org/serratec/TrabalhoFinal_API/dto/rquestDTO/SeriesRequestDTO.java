e8ef6d8d0e50eaba7501185bec50a0188afa0d89
paulocesar-neto <paulo.valeneto@gmail.com>
2026-05-22 15:24:08 -0300

    "Adicionando atributos na entidade SeriesRequestDTO"

--- src/main/java/org/serratec/TrabalhoFinal_API/dto/rquestDTO/SeriesRequestDTO.java
+++ src/main/java/org/serratec/TrabalhoFinal_API/dto/rquestDTO/SeriesRequestDTO.java
@@ -1,4 +1,63 @@
 package org.serratec.TrabalhoFinal_API.dto.rquestDTO;
 
+import java.time.LocalDate;
+
 public class SeriesRequestDTO {
+    private String titulo;
+    private String descricao;
+    private Integer temporadas;
+    private Integer espisodios;
+    private LocalDate dataLancamento;
+    private Double notaMedia;
+
+    public SeriesRequestDTO() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(Integer temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public Integer getEspisodios() {
+        return espisodios;
+    }
+
+    public void setEspisodios(Integer espisodios) {
+        this.espisodios = espisodios;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Double getNotaMedia() {
+        return notaMedia;
+    }
+
+    public void setNotaMedia(Double notaMedia) {
+        this.notaMedia = notaMedia;
+    }
 }
