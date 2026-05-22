cdb0f34805e507741f3ad90ee49fbd869966f927
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-22 17:21:46 -0300

    adicionando metodos vinvular categoria e buscar por categoria

--- src/main/java/org/serratec/TrabalhoFinal_API/services/FilmeService.java
+++ src/main/java/org/serratec/TrabalhoFinal_API/services/FilmeService.java
@@ -1,9 +1,11 @@
 package org.serratec.TrabalhoFinal_API.services;
 
+import org.serratec.TrabalhoFinal_API.domain.Categoria;
 import org.serratec.TrabalhoFinal_API.domain.Filme;
 import org.serratec.TrabalhoFinal_API.dto.request.FilmeRequestDTO;
 import org.serratec.TrabalhoFinal_API.dto.response.FilmeResponseDTO;
 import org.serratec.TrabalhoFinal_API.exceptions.ResourceNotFoundException;
+import org.serratec.TrabalhoFinal_API.repository.CategoriaRepository;
 import org.serratec.TrabalhoFinal_API.repository.FilmeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,9 @@ public class FilmeService {
 
     @Autowired
     private FilmeRepository filmeRepository;
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public List<FilmeResponseDTO> listarFilmes() {
         List<Filme> listaFilmes = filmeRepository.findAll();
@@ -62,10 +67,29 @@ public class FilmeService {
     }
 
     public void deletarFilme(UUID id) {
+
         if (!filmeRepository.existsById(id)) {
             throw new ResourceNotFoundException("Filme não encontrado");
         }
         filmeRepository.deleteById(id);
+    }
+
+    public FilmeResponseDTO vincularCategoria(UUID filmeId, UUID categoriaId) {
+        Filme filme = filmeRepository.findById(filmeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Filme não encontrado"));
+
+        Categoria categoria = categoriaRepository.findById(categoriaId)
+                .orElseThrow(() -> new ResourceNotFoundException("Categoria não encontrada"));
+
+        filme.getCategorias().add(categoria);
+        return new FilmeResponseDTO(filmeRepository.save(filme));
+    }
+
+    public List<FilmeResponseDTO> buscarFilmesPorCategoria(UUID categoriaId) {
+        return filmeRepository.findByCategorias_Id(categoriaId)
+                .stream()
+                .map(FilmeResponseDTO::new)
+                .toList();
     }
 
 }
