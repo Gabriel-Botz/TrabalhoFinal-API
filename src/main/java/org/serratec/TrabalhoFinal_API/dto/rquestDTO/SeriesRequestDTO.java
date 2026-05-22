021b00f8385bb45e7f813f29c16ffe8f9dc5cf50
paulocesar-neto <paulo.valeneto@gmail.com>
2026-05-22 18:34:05 -0300

    "Adicionando anotações @Operation e @ApiResponses e @ApiResponse"

--- src/main/java/org/serratec/TrabalhoFinal_API/dto/rquestDTO/SeriesRequestDTO.java
+++ src/main/java/org/serratec/TrabalhoFinal_API/dto/rquestDTO/SeriesRequestDTO.java
@@ -1,13 +1,34 @@
 package org.serratec.TrabalhoFinal_API.dto.rquestDTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public class SeriesRequestDTO {
+
+    @NotBlank(message = "Titulo é obrigatório")
+    @NotNull(message = "Titulo não pode ser nulo")
     private String titulo;
+
+    @NotBlank(message = "descrição é obrigatório")
+    @NotNull(message = "descrição pode ser nula")
     private String descricao;
+
+    @NotBlank(message = "temporadas é obrigatório")
+    @NotNull(message = "temporadas não pode ser nulo")
     private Integer temporadas;
+
+    @NotBlank(message = "episodios é obrigatório")
+    @NotNull(message = "episodios não pode ser nulo" )
     private Integer espisodios;
+
+    @NotBlank(message = "Data de lançamento é obrigatório")
+    @NotNull(message = "Data de lançamento não pode ser nulo")
     private LocalDate dataLancamento;
+
+    @NotBlank(message = "Nota media é obrigatório")
+    @NotNull(message = "Nota media não pode ser nulo")
     private Double notaMedia;
 
     public SeriesRequestDTO() {
