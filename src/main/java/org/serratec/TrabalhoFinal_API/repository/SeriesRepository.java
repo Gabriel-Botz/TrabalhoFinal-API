418fa2816a5da410b0d2861b07a68adf01d4f96f
paulocesar-neto <paulo.valeneto@gmail.com>
2026-05-22 15:22:32 -0300

    "Adicionando atributos na entidade SeriesResponseDTO"

--- src/main/java/org/serratec/TrabalhoFinal_API/repository/SeriesRepository.java
+++ src/main/java/org/serratec/TrabalhoFinal_API/repository/SeriesRepository.java
@@ -1,4 +1,11 @@
 package org.serratec.TrabalhoFinal_API.repository;
 
-public interface SeriesRepository {
+
+import org.serratec.TrabalhoFinal_API.domain.Series;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SeriesRepository extends JpaRepository<Series,Long> {
+    Series findByTitulo(String titulo);
 }
