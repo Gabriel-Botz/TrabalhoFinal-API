cbd5d75d570d2cf3eb81a18a49b6869a61783c88
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-22 13:22:45 -0300

    criando service de filmes, e metoto criar

--- src/main/java/org/serratec/TrabalhoFinal_API/services/FilmeService.java
+++ src/main/java/org/serratec/TrabalhoFinal_API/services/FilmeService.java
@@ -1,4 +1,28 @@
 package org.serratec.TrabalhoFinal_API.services;
 
+import org.serratec.TrabalhoFinal_API.domain.Filme;
+import org.serratec.TrabalhoFinal_API.dto.request.FilmeRequestDTO;
+import org.serratec.TrabalhoFinal_API.dto.response.FilmeResponseDTO;
+import org.serratec.TrabalhoFinal_API.repository.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FilmeService {
+
+    @Autowired
+    private FilmeRepository filmeRepository;
+
+    public FilmeResponseDTO criarFilme(FilmeRequestDTO dto){
+        var novoFilme = new Filme();
+        novoFilme.setTitulo(dto.getTitulo());
+        novoFilme.setDescricao(dto.getDescricao());
+        novoFilme.setDuracao(dto.getDuracao());
+        novoFilme.setDataLancamento(dto.getDataLancamento());
+        novoFilme.setClassificacaoIndicativa(dto.getClassificacaoIndicativa());
+
+        var filmeSalvo = filmeRepository.save(novoFilme);
+        return new FilmeResponseDTO(filmeSalvo);
+    }
+
 }
