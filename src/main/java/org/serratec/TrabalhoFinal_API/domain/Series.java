f5138c334ee5df5a4ab774dcc170aa9aa11e2b70
paulocesar-neto <paulo.valeneto@gmail.com>
2026-05-22 14:55:16 -0300

    "adicionando atributos na entidade Series"

--- src/main/java/org/serratec/TrabalhoFinal_API/domain/Series.java
+++ src/main/java/org/serratec/TrabalhoFinal_API/domain/Series.java
@@ -1,4 +1,33 @@
 package org.serratec.TrabalhoFinal_API.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "series")
 public class Series {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "titulo",nullable = false)
+    private String titulo;
+
+    @Column(name = "descricao",nullable = false)
+    private String descricao;
+    @Column(name = "temporadas",nullable = false)
+    private Integer temporadas;
+    @Column(name = "episodios",nullable = false)
+    private Integer episodios;
+    @Column(name = "dataLancamento",nullable = false)
+    private LocalDate dataLancamento;
+    @Column(name = "notaMedia",nullable = false)
+    private Double notaMedia;
 }
