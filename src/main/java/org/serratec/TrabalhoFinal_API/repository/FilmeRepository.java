cfde00b92e295c1db38c418b30cf9e4c77bafb10
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-22 13:02:16 -0300

    criando repositorio de filme

--- src/main/java/org/serratec/TrabalhoFinal_API/repository/FilmeRepository.java
+++ src/main/java/org/serratec/TrabalhoFinal_API/repository/FilmeRepository.java
@@ -1,4 +1,12 @@
 package org.serratec.TrabalhoFinal_API.repository;
 
-public interface FilmeRepository {
+import org.hibernate.validator.constraints.UUID;
+import org.serratec.TrabalhoFinal_API.domain.Filme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FilmeRepository extends JpaRepository<Filme, UUID> {
+
+
 }
