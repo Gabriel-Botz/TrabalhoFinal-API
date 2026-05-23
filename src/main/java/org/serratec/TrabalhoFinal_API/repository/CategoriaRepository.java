6a232cd95b0eb1d7bfcd2e44a3b26836a03bae57
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-23 09:25:22 -0300

    adicionando imports necessarios

--- src/main/java/org/serratec/TrabalhoFinal_API/repository/CategoriaRepository.java
+++ src/main/java/org/serratec/TrabalhoFinal_API/repository/CategoriaRepository.java
@@ -4,7 +4,11 @@ import org.serratec.TrabalhoFinal_API.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 
+    Optional<Object> findById(UUID categoriaId);
 }
