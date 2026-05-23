bb01969c1eae3de5c77db9aa230ccec51761affe
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-05-23 16:31:14 -0300

    fixed(ConfigSeguranca): correção dos caminhas das listas, para usuário logado e para visualização das listas considerado como "disponivel" para visualização

--- src/main/java/org/serratec/trabalho_final_api/config/ConfigSeguranca.java
+++ src/main/java/org/serratec/trabalho_final_api/config/ConfigSeguranca.java
@@ -1,17 +1,15 @@
 package org.serratec.trabalho_final_api.config;
 
-import java.beans.Customizer;
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -23,24 +21,18 @@ public class ConfigSeguranca {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .cors((cors) -> cors.configurationSource((configurationSource())))
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(configurationSource()))
                 .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(
-                        // /lista-favoritos/publica -> lista para listar usuario e lista de favoritos
-                        // dele adicionado como publico
-                        request -> {
-                            request.requestMetchers(HttpMethod.GET, "/lista-favoritos/filmes/publica")
-                                    .permitAll();
-
-                            request.requestMetchers(HttpMethod.GET, "/lista-favoritos/series/publica")
-                                    .permitAll();
-
-                            request.requestMetchers(HttpMethod.GET, "/usuarios/" + usuario.getId + "/lista")
-                                    .anyRequest()
-                                    .authentication();
-                        })
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers(HttpMethod.GET, "/lista-favoritos/filmes/publica").permitAll();
+                    request.requestMatchers(HttpMethod.GET, "/lista-favoritos/series/publica").permitAll();
+                    request.requestMatchers(HttpMethod.GET, "/usuarios/**").authenticated();
+                    request.anyRequest().authenticated(); // para qualquer outra requisição
+                })
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return http.build();
     }
 
@@ -50,10 +42,16 @@ public class ConfigSeguranca {
     // }
 
     @Bean
-    private CorsConfigurationSource configurationSource() {
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:6000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
         return source;
