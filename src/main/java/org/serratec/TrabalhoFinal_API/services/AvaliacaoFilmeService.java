37a0cd3ab236a4f577154995a5de70094ff048cc
elionardosantos <elionars@gmail.com>
2026-05-23 10:23:41 -0300

    fix: corrigida importação de RecursoNaoEncontrado

--- src/main/java/org/serratec/TrabalhoFinal_API/services/AvaliacaoFilmeService.java
+++ src/main/java/org/serratec/TrabalhoFinal_API/services/AvaliacaoFilmeService.java
@@ -10,7 +10,7 @@ import org.serratec.TrabalhoFinal_API.domain.Filme;
 import org.serratec.TrabalhoFinal_API.domain.Usuario;
 import org.serratec.TrabalhoFinal_API.dto.request.AvaliacaoFilmeRequestDTO;
 import org.serratec.TrabalhoFinal_API.dto.response.AvaliacaoFilmeResponseDTO;
-import org.serratec.TrabalhoFinal_API.exception.ErroResposta;
+import org.serratec.TrabalhoFinal_API.exception.RecursoNaoEncontradoException;
 import org.serratec.TrabalhoFinal_API.repository.AvaliacaoFilmeRepository;
 import org.serratec.TrabalhoFinal_API.repository.FilmeRepository;
 import org.serratec.TrabalhoFinal_API.repository.UsuarioRepository;
