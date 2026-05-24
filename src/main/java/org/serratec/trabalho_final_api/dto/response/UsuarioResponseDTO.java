0c3e5662c5ffb8ea558f7328d932969898880ec4
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-05-24 20:59:50 -0300

    fixed: correção nas classes Usuario, UsuarioResponseDTO e ConfigSeguranca

--- src/main/java/org/serratec/trabalho_final_api/dto/response/UsuarioResponseDTO.java
+++ src/main/java/org/serratec/trabalho_final_api/dto/response/UsuarioResponseDTO.java
@@ -9,6 +9,8 @@ import org.serratec.trabalho_final_api.enumerated.TipoUsuario;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
 @Schema(description = "Modelo de resposta com os dados detalhados do usuário")
 @JsonPropertyOrder({ "id", "nome", "email", "username", "dataCriacao" })
@@ -17,7 +19,7 @@ public record UsuarioResponseDTO(
         @Schema(description = "Nome do usuario cadastrado", example = "Luna") String nome,
         @Schema(description = "Endereço de e-mail cadastrado", example = "marcia.silva@email.com") String email,
         @Schema(description = "Login do usuario cadastrado", example = "LinaPhon") String username,
-        @Schema(description = "Tipo de usurio cadastrado", example = "ADMIN") TipoUsuario tipoUsuario,
+        @Schema(description = "Tipo de usurio cadastrado", example = "ADMIN") @Enumerated(EnumType.STRING) TipoUsuario tipoUsuario,
         @Schema(description = "Data e Horário da criação da conta", example = "13-03-2026T00:00") LocalDateTime dataCriacao,
         @Schema(description = "Endereço de imagem cadastrado", example = "localhost://8080/mainha.png") String fotoPerfil) {
 
