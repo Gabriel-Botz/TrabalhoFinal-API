d88b63e6795f3de28e5153032aad73772a9b2c94
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-05-25 19:51:52 -0300

    criação do DTO de detalhes

--- src/main/java/org/serratec/trabalho_final_api/dto/response/TmdbDetalhesDTO.java
+++ src/main/java/org/serratec/trabalho_final_api/dto/response/TmdbDetalhesDTO.java
@@ -1,4 +1,46 @@
 package org.serratec.trabalho_final_api.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TmdbDetalhesDTO {
+
+    @JsonProperty("runtime")
+    private Integer runtime;
+
+    @JsonProperty("release_dates")
+    private ReleaseDatesContainer releaseDates;
+
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class ReleaseDatesContainer {
+        @JsonProperty("results")
+        private List<PaisResult> results;
+    }
+
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class PaisResult {
+        @JsonProperty("iso_3166_1")
+        private String isoCodigo;
+
+        @JsonProperty("release_dates")
+        private List<CertificacaoItem> releaseDates;
+    }
+
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class CertificacaoItem {
+        @JsonProperty("certification")
+        private String certification;
+    }
 }
