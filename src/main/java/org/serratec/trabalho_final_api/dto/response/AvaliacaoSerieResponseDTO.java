384945f052add40d062aa04af5337df5f0aa43e0
vLamass <viniciusslamas17@gmail.com>
2026-05-25 16:01:23 -0300

    Fazendo o Restando do Swagger de Avaliação Serie e Avaliação Filme

--- src/main/java/org/serratec/trabalho_final_api/dto/response/AvaliacaoSerieResponseDTO.java
+++ src/main/java/org/serratec/trabalho_final_api/dto/response/AvaliacaoSerieResponseDTO.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.serratec.trabalho_final_api.domain.AvaliacaoSerie;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,16 +13,22 @@ import lombok.Setter;
 @Setter
 public class AvaliacaoSerieResponseDTO {
 
+    @Schema(description = "ID de avaliação da série", example = "540e8400-e29b-41d4-a716-446655440000")
     private UUID id;
 
+    @Schema(description = "Nome do usuário que realizou a avaliação, example = Paulo Victor")
     private String nomeUsuario;
 
+    @Schema(description = "Nome da série avaliada", example = "Reacher")
     private String nomeSerie;
 
+    @Schema(description = "Nota atribuida à série", example = "9.0")
     private Double nota;
 
+    @Schema(description = "Comentário da avaliação", example = "Excelente roteiro e atuação.")
     private String comentario;
 
+    @Schema(description = "Data da avaliação", example = "2026-05-25")
     private LocalDate dataAvaliacao;
 
     public AvaliacaoSerieResponseDTO(AvaliacaoSerie avaliacao) {
