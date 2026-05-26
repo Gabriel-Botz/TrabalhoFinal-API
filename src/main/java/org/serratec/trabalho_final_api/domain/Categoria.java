bb9076d0f39816e0edda5e904296d9332a68af23
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-05-26 16:28:46 -0300

    fixed: Rafa vi que não fez o getter/setter de filmes e series

--- src/main/java/org/serratec/trabalho_final_api/domain/Categoria.java
+++ src/main/java/org/serratec/trabalho_final_api/domain/Categoria.java
@@ -9,37 +9,69 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "categorias")
 public class Categoria {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Column(nullable = false, unique=true, length = 40)
+
+	@Column(nullable = false, unique = true, length = 40)
 	private String nome;
-	
+
 	@Column(nullable = true, length = 300)
 	private String descricao;
 
 	@ManyToMany(mappedBy = "categorias")
-    @JsonIgnore
-    private List<Filme> filmes;
+	@JsonIgnore
+	private List<Filme> filmes;
 
-    @ManyToMany(mappedBy = "categorias")
-    @JsonIgnore 
-    private List<Series> series;
+	@ManyToMany(mappedBy = "categorias")
+	@JsonIgnore
+	private List<Series> series;
 
-	//Construtor Vazio ↓
+	// Construtor Vazio ↓
 	public Categoria() {
 		super();
 	}
-	
-	//Getters e Setters ↓
-	public Long getId() {return id;}
-	public void setId(Long id) {this.id = id;}
 
-	public String getNome() {return nome;}
-	public void setNome(String nome) {this.nome = nome;}
+	// Getters e Setters ↓
+	public Long getId() {
+		return id;
+	}
 
-	public String getDescricao() {return descricao;}
-	public void setDescricao(String descricao) {this.descricao = descricao;}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Filme> getFilmes() {
+		return filmes;
+	}
+
+	public void setFilmes(List<Filme> filmes) {
+		this.filmes = filmes;
+	}
+
+	public List<Series> getSeries() {
+		return series;
+	}
+
+	public void setSeries(List<Series> series) {
+		this.series = series;
+	}
+
 }
