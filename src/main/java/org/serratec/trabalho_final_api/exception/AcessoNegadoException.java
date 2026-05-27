353750b1b58194427fa6dfd0882e86fed012ebb4
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-05-27 13:39:27 -0300

    fix: Correção do exception -> tipo de erro

--- src/main/java/org/serratec/trabalho_final_api/exception/AcessoNegadoException.java
+++ src/main/java/org/serratec/trabalho_final_api/exception/AcessoNegadoException.java
@@ -3,7 +3,7 @@ package org.serratec.trabalho_final_api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.FORBIDDEN)
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class AcessoNegadoException extends RuntimeException {
 
     public AcessoNegadoException(String message) {
