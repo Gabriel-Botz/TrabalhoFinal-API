4d6e8cbeef11fd102c49fe672d1b4a7a9837b636
vLamass <viniciusslamas17@gmail.com>
2026-05-27 09:20:24 -0300

    Criação do top5 para Series

--- src/main/java/org/serratec/trabalho_final_api/repository/AvaliacaoSerieRepository.java
+++ src/main/java/org/serratec/trabalho_final_api/repository/AvaliacaoSerieRepository.java
@@ -1,10 +1,11 @@
 package org.serratec.trabalho_final_api.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.serratec.trabalho_final_api.domain.AvaliacaoSerie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AvaliacaoSerieRepository extends JpaRepository<AvaliacaoSerie, UUID> {
-
+    List<AvaliacaoSerie> findAllByOrderByNotaDesc();
 }
